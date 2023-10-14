3af14667518309e701a7f4b240d48aabecf26258
PopekaDS <70101127+PopekaDS@users.noreply.github.com>
2023-10-14 14:15:59 +0400

    Add files via upload

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -7,17 +7,7 @@ import sys
 
 
 def main():
-    print("""The Tower of Hanoi, by Al Sweigart al@inventwithpython.com
 
-Move the tower of disks, one disk at a time, to another tower. Larger
-disks cannot rest on top of a smaller disk.
-
-More info at https://en.wikipedia.org/wiki/Tower_of_Hanoi
-"""
-    )
-
-    # Set up the towers. The end of the list is the top of the tower.
-    towers = {'A': copy.copy(COMPLETE_TOWER), 'B': [], 'C': []}
 
     while True:  # Run a single turn.
         # Display the towers and disks:
@@ -37,42 +27,10 @@ More info at https://en.wikipedia.org/wiki/Tower_of_Hanoi
             sys.exit()
 
 
-def askForPlayerMove(towers):
-    """Asks the player for a move. Returns (fromTower, toTower)."""
-
-    while True:  # Keep asking player until they enter a valid move.
-        print('Enter the letters of "from" and "to" towers, or QUIT.')
-        print('(e.g. AB to moves a disk from tower A to tower B.)')
-        response = input('> ').upper().strip()
-
-        if response == 'QUIT':
-            print('Thanks for playing!')
-            sys.exit()
-
-        # Make sure the user entered valid tower letters:
-        if response not in ('AB', 'AC', 'BA', 'BC', 'CA', 'CB'):
-            print('Enter one of AB, AC, BA, BC, CA, or CB.')
-            continue  # Ask player again for their move.
-
-        # Syntactic sugar - Use more descriptive variable names:
-        fromTower, toTower = response[0], response[1]
-
-        if len(towers[fromTower]) == 0:
-            # The "from" tower cannot be an empty tower:
-            print('You selected a tower with no disks.')
-            continue  # Ask player again for their move.
-        elif len(towers[toTower]) == 0:
-            # Any disk can be moved onto an empty "to" tower:
-            return fromTower, toTower
-        elif towers[toTower][-1] < towers[fromTower][-1]:
-            print('Can\'t put larger disks on top of smaller ones.')
-            continue  # Ask player again for their move.
-        else:
-            # This is a valid move, so return the selected towers:
-            return fromTower, toTower
 
 
-def displayTowers(towers):
+
+def xcfc(towers):
     """Display the current state."""
 
     # Display the three towers:
@@ -114,11 +72,26 @@ if __name__ == '__main__':
 
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
+void askForPlayerMove(map<char, vector<int>> towers, char& fromTower, char& toTower) {
+    // Asks the player for a move. Returns (fromTower, toTower).
+    while (1) { // Keep asking player until they enter a valid move.
+        std::cout << "Enter the letters of 'from' and 'to' towers, or QUIT.\n;";
+        std::cout << "(e.g. AB to moves a disk from tower A to tower B.)\n";
+        std::cout << "> ";
+        string response;
+        cin >> response;
+        // response.upper() // необходимо дописать
 
-
-
+        if (response == "QUIT") {
+            cout << "Thanks for playing!\n";
+            // sys.exit() // необходимо дописать
+        }
+    }
+}
+            
 
 int main() {
     int TOTAL_DISKS = 5; // More disks means a more difficult puzzle.
@@ -129,6 +102,50 @@ int main() {
         COMPLETE_TOWER[i] = TOTAL_DISKS - i;
     }
 
+    cout << "The Tower of Hanoi, by Al Sweigart al@inventwithpython.com\n\n";
+    cout << "Move the tower of disks, one disk at a time, to another tower. Larger\n";
+    cout << "disks cannot rest on top of a smaller disk.\n\n";
+    cout << "The Tower of Hanoi, by Al Sweigart al@inventwithpython.com\n";
+    cout << "More info at https://en.wikipedia.org/wiki/Tower_of_Hanoi\n";
 
+    // Set up the towers.The end of the list is the top of the tower.
+    map<char, vector<int>> towers = {
+        {'A', COMPLETE_TOWER}, {'B', {}}, {'C', {}} // // // towers = { 'A': copy.copy(COMPLETE_TOWER), 'B' : [] , 'C' : [] }
+    };
+
+
+
+
+    
     return 0;
-}
+} 
+
+def askForPlayerMove(towers):
+    
+
+    while True:  # 
+
+
+
+
+        # Make sure the user entered valid tower letters:
+        if response not in ('AB', 'AC', 'BA', 'BC', 'CA', 'CB'):
+            print('Enter one of AB, AC, BA, BC, CA, or CB.')
+            continue  # Ask player again for their move.
+
+        # Syntactic sugar - Use more descriptive variable names:
+        fromTower, toTower = response[0], response[1]
+
+        if len(towers[fromTower]) == 0:
+            # The "from" tower cannot be an empty tower:
+            print('You selected a tower with no disks.')
+            continue  # Ask player again for their move.
+        elif len(towers[toTower]) == 0:
+            # Any disk can be moved onto an empty "to" tower:
+            return fromTower, toTower
+        elif towers[toTower][-1] < towers[fromTower][-1]:
+            print('Can\'t put larger disks on top of smaller ones.')
+            continue  # Ask player again for their move.
+        else:
+            # This is a valid move, so return the selected towers:
+            return fromTower, toTower
