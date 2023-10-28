00f801e2575037b924892102fbbaa47b8845255c
PopekaDS <70101127+PopekaDS@users.noreply.github.com>
2023-10-28 18:57:56 +0400

    Add files via upload

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -1,9 +1,10 @@
 /*
+The Tower of Hanoi, by Al Sweigart al@inventwithpython.com
+A stack-moving puzzle game.
+This code is available at https://nostarch.com/big-book-small-python-programming
 
 import copy
 import sys
-
-
 
 
 def main():
@@ -27,106 +28,17 @@ def main():
             sys.exit()
 
 
-
-
-
-def xcfc(towers):
-    """Display the current state."""
-
-    # Display the three towers:
-    for level in range(TOTAL_DISKS, -1, -1):
-        for tower in (towers['A'], towers['B'], towers['C']):
-            if level >= len(tower):
-                displayDisk(0)  # Display the bare pole with no disk.
-            else:
-                displayDisk(tower[level])  # Display the disk.
-        print()
-
-    # Display the tower labels A, B, and C.
-    emptySpace = ' ' * (TOTAL_DISKS)
-    print('{0} A{0}{0} B{0}{0} C\n'.format(emptySpace))
-
-
-def displayDisk(width):
-    """Display a disk of the given width. A width of 0 means no disk."""
-    emptySpace = ' ' * (TOTAL_DISKS - width)
-
-    if width == 0:
-        # Display a pole segment without a disk:
-        print(emptySpace + '||' + emptySpace, end='')
-    else:
-        # Display the disk:
-        disk = '@' * width
-        numLabel = str(width).rjust(2, '_')
-        print(emptySpace + disk + numLabel + disk + emptySpace, end='')
-
-
-# If the program is run (instead of imported), run the game:
-if __name__ == '__main__':
-    main()
-*/
-
-// The Tower of Hanoi, by Al Sweigart al@inventwithpython.com
-// A stack - moving puzzle game.
-// This code is available at https ://nostarch.com/big-book-small-python-programming
-
-#include <iostream>
-#include <vector>
-#include <map>
-using namespace std;
-
-void askForPlayerMove(map<char, vector<int>> towers, char& fromTower, char& toTower) {
-    // Asks the player for a move. Returns (fromTower, toTower).
-    while (1) { // Keep asking player until they enter a valid move.
-        std::cout << "Enter the letters of 'from' and 'to' towers, or QUIT.\n;";
-        std::cout << "(e.g. AB to moves a disk from tower A to tower B.)\n";
-        std::cout << "> ";
-        string response;
-        cin >> response;
-        // response.upper() // необходимо дописать
-
-        if (response == "QUIT") {
-            cout << "Thanks for playing!\n";
-            // sys.exit() // необходимо дописать
-        }
-    }
-}
-            
-
-int main() {
-    int TOTAL_DISKS = 5; // More disks means a more difficult puzzle.
-
-    // Start with all disks on tower A :
-    vector <int> COMPLETE_TOWER(TOTAL_DISKS);
-    for (int i = 0; i < TOTAL_DISKS; ++i) {
-        COMPLETE_TOWER[i] = TOTAL_DISKS - i;
-    }
-
-    cout << "The Tower of Hanoi, by Al Sweigart al@inventwithpython.com\n\n";
-    cout << "Move the tower of disks, one disk at a time, to another tower. Larger\n";
-    cout << "disks cannot rest on top of a smaller disk.\n\n";
-    cout << "The Tower of Hanoi, by Al Sweigart al@inventwithpython.com\n";
-    cout << "More info at https://en.wikipedia.org/wiki/Tower_of_Hanoi\n";
-
-    // Set up the towers.The end of the list is the top of the tower.
-    map<char, vector<int>> towers = {
-        {'A', COMPLETE_TOWER}, {'B', {}}, {'C', {}} // // // towers = { 'A': copy.copy(COMPLETE_TOWER), 'B' : [] , 'C' : [] }
-    };
-
-
-
-
-    
-    return 0;
-} 
-
 def askForPlayerMove(towers):
-    
+    """Asks the player for a move. Returns (fromTower, toTower)."""
 
-    while True:  # 
+    while True:  # Keep asking player until they enter a valid move.
+        print('Enter the letters of "from" and "to" towers, or QUIT.')
+        print('(e.g. AB to moves a disk from tower A to tower B.)')
+        response = input('> ').upper().strip()
 
-
-
+        if response == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
 
         # Make sure the user entered valid tower letters:
         if response not in ('AB', 'AC', 'BA', 'BC', 'CA', 'CB'):
@@ -149,3 +61,72 @@ def askForPlayerMove(towers):
         else:
             # This is a valid move, so return the selected towers:
             return fromTower, toTower
+
+
+def displayTowers(towers):
+    """Display the current state."""
+
+    # Display the three towers:
+    for level in range(TOTAL_DISKS, -1, -1):
+        for tower in (towers['A'], towers['B'], towers['C']):
+            if level >= len(tower):
+                displayDisk(0)  # Display the bare pole with no disk.
+            else:
+                displayDisk(tower[level])  # Display the disk.
+        print()
+
+    # Display the tower labels A, B, and C.
+    emptySpace = ' ' * (TOTAL_DISKS)
+    print('{0} A{0}{0} B{0}{0} C\n'.format(emptySpace))
+
+
+*/
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+int TOTAL_DISKS = 5; // More disks means a more difficult puzzle.
+void displayDisk(int width);
+
+int main() {
+    cout << "The Tower of Hanoi, by Al Sweigart al@inventwithpython.com\n";
+    cout << "Move the tower of disks, one disk at a time, to another tower.Larger\n";
+    cout << "disks cannot rest on top of a smaller disk.\n";
+    cout << "More info at https ://en.wikipedia.org/wiki/Tower_of_Hanoi\n";
+
+    // Start with all disks on tower A :
+    vector <int> COMPLETE_TOWER(TOTAL_DISKS);
+    for (int i = 0; i < TOTAL_DISKS; ++i) {
+        COMPLETE_TOWER[i] = TOTAL_DISKS - i;
+    }
+
+    // Set up the towers.The end of the list is the top of the tower.
+    map<char, vector<int>> towers = {
+        {'A', COMPLETE_TOWER}, {'B', {}}, {'C', {}} // // // towers = { 'A': copy.copy(COMPLETE_TOWER), 'B' : [] , 'C' : [] }
+    };
+
+    displayDisk(0);
+    displayDisk(1);
+    displayDisk(2);
+    displayDisk(3);
+    displayDisk(4);
+    displayDisk(5);
+
+    return 0;
+}
+
+void displayDisk(int width) {
+    // Display a disk of the given width. A width of 0 means no disk.
+    string emptySpace(TOTAL_DISKS - width, ' ');
+    if (width == 0) {
+        cout << emptySpace << "||" << emptySpace << '\n';
+    } else {
+        // Display the disk:
+        string disk(width, '@');
+        string numLabel = "_" + to_string(width);
+        cout << emptySpace << disk << numLabel << disk << emptySpace << '\n';
+    }
+}
